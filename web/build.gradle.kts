688a2ca9de8f9b40e0d0848ef6b837cfc79ce0fb
CarloMicieli <carlo.micieli@gmail.com>
2022-07-06 22:57:52 +0200

    build: setup integration tests

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
 import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
@@ -16,4 +20,45 @@ dependencies {
 
 tasks.getByName<BootRun>("bootRun") {
     mainClass.set("com.trenako.ApplicationKt")
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+
+        val integrationTest by registering(JvmTestSuite::class) {
+            dependencies {
+                implementation(project)
+                implementation("org.springframework.boot:spring-boot-starter-test")
+            }
+
+            targets {
+                all {
+                    testTask.configure {
+                        shouldRunAfter(test)
+                        minHeapSize = "1G"
+                        maxHeapSize = "2G"
+
+                        failFast = false
+                        ignoreFailures = true
+
+                        testLogging {
+                            showStandardStreams = false
+                            events(PASSED, FAILED, SKIPPED)
+                            showExceptions = true
+                            showCauses = true
+                            showStackTraces = true
+                            exceptionFormat = FULL
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+tasks.named("check") {
+    dependsOn(testing.suites.named("integrationTest"))
 }
