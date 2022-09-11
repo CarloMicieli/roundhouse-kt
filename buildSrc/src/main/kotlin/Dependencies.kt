4038f225179c145b38611561bce72f785c354ef4
CarloMicieli <carlo.micieli@gmail.com>
2022-09-11 10:22:22 +0200

    build: upgrade postgresql r2dbc driver

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,5 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
+object R2dbcPostgresql {
+    val version = "0.8.13.RELEASE"
+}
--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.liquibase:liquibase-core")
     implementation("org.springframework:spring-jdbc")
-    runtimeOnly("io.r2dbc:r2dbc-postgresql:0.8.12.RELEASE")
+    runtimeOnly("io.r2dbc:r2dbc-postgresql:${R2dbcPostgresql.version}")
     runtimeOnly("org.postgresql:postgresql")
 }
 
