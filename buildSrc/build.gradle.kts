e242c78f75e1e6eed2d2af21c792472334eeceed
CarloMicieli <carlo.micieli@gmail.com>
2022-12-27 19:01:08 +0100

    build: bump gradle plugin versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,12 +8,12 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.11.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.12.0")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
-    implementation("me.qoomon:gradle-git-versioning-plugin:6.3.6")
+    implementation("me.qoomon:gradle-git-versioning-plugin:6.3.7")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.7.21")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.0")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.1")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.44.0")
     implementation("org.jetbrains.kotlinx:kover:0.6.1")
 }
