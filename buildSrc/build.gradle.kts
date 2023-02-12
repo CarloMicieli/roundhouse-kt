37811dc8e7bca62404c2e1f7d6f371019358ad2d
CarloMicieli <carlo.micieli@gmail.com>
2023-02-12 15:42:51 +0100

    build: update gradle plugin versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,16 +4,16 @@ plugins {
 
 repositories {
     gradlePluginPortal()
-    maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.12.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.14.0")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.0")
-    implementation("me.qoomon:gradle-git-versioning-plugin:6.3.7")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.8.0")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.1")
-    implementation("com.github.ben-manes:gradle-versions-plugin:0.44.0")
+    implementation("me.qoomon:gradle-git-versioning-plugin:6.4.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:1.8.10")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.0.2")
+    implementation("com.github.ben-manes:gradle-versions-plugin:0.45.0")
     implementation("org.jetbrains.kotlinx:kover:0.6.1")
+    implementation("org.graalvm.buildtools.native:org.graalvm.buildtools.native.gradle.plugin:0.9.19")
 }
