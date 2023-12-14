ead2a40d66782bb66e67ea0be5a12ddbdae42e8f
CarloMicieli <carlo.micieli@gmail.com>
2023-12-14 20:45:08 +0100

    build: upgrade to spring boot 3.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("me.qoomon:gradle-git-versioning-plugin:6.4.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.20")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.0")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.2.0")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.46.0")
     implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.1")
     implementation("org.openapitools:openapi-generator-gradle-plugin:6.6.0")
